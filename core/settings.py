e151192bb43332140c1db13d563b399f23695aee
Asim Nepal <gnepal80m@gmail.com>
2022-02-10 23:59:14 +0545

    Added share_link related models

--- core/settings.py
+++ core/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 
     'user',
     'document',
+    'share_link',
 ]
 
 
--- share_link/models.py
+++ share_link/models.py
@@ -1,12 +1,48 @@
+import os
 import uuid
 from django.conf import settings
 from django.db import models
 from document.models import Document, DocumentImage
+from django.utils import timezone
 
 
 class ShareLink(models.Model):
+    """Share link"""
     id = models.UUIDField(primary_key=True,
                           default=uuid.uuid4, editable=False, auto_created=True, unique=True,)
     sender = models.ForeignKey(settings.AUTH_USER_MODEL,
                                on_delete=models.CASCADE)
-    expiry_date = models.DateField()
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    expiry_date = models.DateTimeField(
+        default=timezone.now()+timezone.timedelta(days=7))
+
+
+class SharedDocument(models.Model):
+    """Shared Identity Document"""
+
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL,
+    #                          on_delete=models.CASCADE)
+    share_link = models.ForeignKey(
+        ShareLink, on_delete=models.CASCADE, related_name='documents', )
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+def document_image_file_path(instance, filename):
+    """Generate file path for new document image"""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return os.path.join(f'uploads/shared_document/{instance.document.id}', filename)
+
+
+class SharedDocumentImage(models.Model):
+    """Image of document object"""
+    document = models.ForeignKey(
+        SharedDocument, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to=document_image_file_path)
+
+    def __str__(self):
+        return f'{self.document.title}-{self.id}'
