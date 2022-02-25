bc707b00f7356ba8db753f1049f9f09c6a5748eb
Asim Nepal <gnepal80m@gmail.com>
2022-02-25 17:06:34 +0545

    patch

--- core/settings.py
+++ core/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static/'
 # STATICFILES_DIRS = (
 # BASE_DIR / 'static/admin',
