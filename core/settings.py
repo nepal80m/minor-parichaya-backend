0b47e340ed95835d6caa1151b180fadc149e45f6
Asim Nepal <gnepal80m@gmail.com>
2022-02-25 16:50:42 +0545

    removed static dirs

--- core/settings.py
+++ core/settings.py
@@ -142,9 +142,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-)
+# STATICFILES_DIRS = (
+#     BASE_DIR / 'static',
+# )
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
